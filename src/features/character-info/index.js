a0b260380a56539a97e8eab1676f50ecf0d94995
Mark Tellez <mark.tellez@icloud.com>
2018-03-18 11:49:55 -0600

    character info

--- src/features/character-info/index.js
+++ src/features/character-info/index.js
@@ -1,6 +1,29 @@
 import React from 'react'
+import './styles.css'
 
 export default function CharacterInfo(props) {
   return <div>
+    <h2>Character info</h2>
+    <div className='character-info'>
+      <div>
+        <label for='charcter-name'>Name</label>
+        <input name='charcter-name'
+          value={props.characterName}
+          onChange={props.characterNameChanged}
+        />
+      </div>
+
+      <div>
+        <label for='charcter-bio'>Bio</label>
+        <textarea name='charcter-bio'
+          value={props.characterBio}
+          onChange={props.characterBioChanged}
+        />
+      </div>
+
+      <div className='button'>
+        <button>Save</button>
+      </div>
+    </div>
   </div>
 }
